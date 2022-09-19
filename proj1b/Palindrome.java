13137ba4e04316a03d0acee2974a94912e5a1bcb
PancakeZoy <pancakezoy@gmail.com>
2022-09-19 03:12:14 -0400

    proj1b use LinkedDeque instead

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,6 +1,6 @@
 public class Palindrome {
     public Deque<Character> wordToDeque(String word){
-        Deque<Character> wordDeque = new ArrayDeque<Character> ();
+        Deque<Character> wordDeque = new LinkedListDeque<> ();
         for (int i=0; i<word.length(); i++){
             wordDeque.addLast(word.charAt(i));
         }
