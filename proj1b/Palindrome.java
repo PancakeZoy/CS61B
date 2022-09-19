2f599c1e47540aebd71167c08bde1cfb7997944d
PancakeZoy <pancakezoy@gmail.com>
2022-09-19 03:21:25 -0400

    add some new test to offBYONE

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,6 +1,6 @@
 public class Palindrome {
     public Deque<Character> wordToDeque(String word){
-        Deque<Character> wordDeque = new LinkedListDeque<> ();
+        Deque<Character> wordDeque = new ArrayDeque<Character> ();
         for (int i=0; i<word.length(); i++){
             wordDeque.addLast(word.charAt(i));
         }
